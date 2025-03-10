1510f0a35c24f8e044d88e111bc9986d547eef91
evenrygh99 <even.rygh@outlook.com>
2025-03-10 13:54:06 +0100

    lagt til kode i test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, GitHub!");
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Welcome, {name}!");
+    }
+}
